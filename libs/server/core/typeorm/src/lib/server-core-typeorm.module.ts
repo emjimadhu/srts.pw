ef52fe0588dc07e27e37d4102e1ac9515445f6c2
Em Ji Madhu <emjimadhu@gmail.com>
2020-06-20 21:51:56 +0530

    feat(server): :sparkles: add `typeorm` config

--- libs/server/core/typeorm/src/lib/server-core-typeorm.module.ts
+++ libs/server/core/typeorm/src/lib/server-core-typeorm.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getMetadataArgsStorage } from 'typeorm';
+import { TableMetadataArgs } from 'typeorm/metadata-args/TableMetadataArgs';
+
+import { environment } from '@srts.pw/server/environments';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: []
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: environment.mongoUri,
+      useUnifiedTopology: true,
+      appname: 'srtspw',
+      logging: 'all',
+      synchronize: true,
+      /*
+        Fix for "NoEntityMetadata Found"
+        link: https://github.com/nrwl/nx/issues/1393#issuecomment-526135967
+      */
+      entities: getMetadataArgsStorage().tables.map((table: TableMetadataArgs) => table.target)
+    })
+  ]
 })
 export class ServerCoreTypeormModule {}
