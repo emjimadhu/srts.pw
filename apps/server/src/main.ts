1e25466635666f6a6e1bb32416f7d22e5117881a
Em Ji Madhu <emjimadhu@gmail.com>
2020-06-21 20:36:28 +0530

    chore(server): :heavy_plus_sign: add `class-{validator,transformer}` dependencies

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,4 +1,6 @@
-import { Logger } from '@nestjs/common';
+import {
+  Logger, ValidationPipe
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { environment } from '@srts.pw/server/environments';
@@ -7,6 +9,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const logger = new Logger('Bootstraap');
   await app.listen(environment.port, () => {
     logger.log(`Server running on http://localhost:${environment.port}`);
