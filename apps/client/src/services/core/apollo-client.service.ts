4e68e40cd8ef89d79356ecbe12f44174f91801bc
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-09 22:56:01 +0530

    feat(libs): :sparkles: add `environment` config for client

--- apps/client/src/services/core/apollo-client.service.ts
+++ apps/client/src/services/core/apollo-client.service.ts
@@ -2,15 +2,17 @@ import {
   ApolloClient, InMemoryCache, createHttpLink, NormalizedCacheObject
 } from '@apollo/client';
 
+import { environment } from '@srts.pw/client/environments';
+
 export const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   link: createHttpLink({
-    uri: 'http://localhost:3001/graphql'
+    uri: environment.graphQlURL
   }),
   cache: new InMemoryCache(),
   name: 'srts.pw-wweb-client',
   version: '1.3',
   queryDeduplication: false,
-  connectToDevTools: true,
+  connectToDevTools: !environment.production,
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'cache-and-network',
