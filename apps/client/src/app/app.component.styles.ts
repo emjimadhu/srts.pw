09f6dc79dcf8f4f3cd0612bff3950c3b230d3d6f
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-10 01:18:58 +0530

    style(client): :lipstick: use fluent ui design system

---
+++ apps/client/src/app/app.component.styles.ts
@@ -0,0 +1,13 @@
+import {
+  ITextTokens, FontWeights, IStackTokens
+} from '@fluentui/react';
+
+export const boldStyle: ITextTokens = {
+  root: {
+    fontWeight: FontWeights.semibold
+  }
+};
+
+export const stackHorizontalGap: IStackTokens = {
+  childrenGap: 15
+};
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import {
+  Customizer, mergeStyles, Customizations
+} from '@fluentui/react';
 
 import AppComponent from './app/app.component';
+
+mergeStyles({
+  selectors: {
+    ':global(body), :global(html), :global(#root)': {
+      margin: 0,
+      padding: 0,
+      height: '100vh'
+    }
+  }
+});
 
 
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppComponent />
+      <Customizer {...Customizations}>
+        <AppComponent />
+      </Customizer>
     </BrowserRouter>
   </React.StrictMode>,
   document.querySelector('#root')
