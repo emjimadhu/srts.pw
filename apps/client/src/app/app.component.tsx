6881478b27ebd1ff2b0133da55cbe76eb493466d
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-15 08:08:20 +0530

    refactor(client): :recycle: remove `fluent-ui` configs from main and app.tsx

--- apps/client/src/app/app.component.tsx
+++ apps/client/src/app/app.component.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { Stack } from '@fluentui/react';
 
 import { apolloClient } from '../services/core/apollo-client.service';
-import { stackHorizontalGap } from './app.styles';
 import {
   getUser, setUser
 } from '../services/core/auth.service';
-import AppBarComponent from '../components/app/app-bar/app-bar.component';
-import CreateShortUrlComponent from '../components/app/create-short-url/create-short-url.component';
 
 
 const AppComponent: React.FC = () => {
@@ -18,23 +14,7 @@ const AppComponent: React.FC = () => {
 
   return (
     <ApolloProvider client={apolloClient}>
-      <AppBarComponent />
-      <Stack
-        horizontalAlign="center"
-        verticalAlign="center"
-        verticalFill
-        styles={{
-          root: {
-            width: '100vw',
-            height: '100vh',
-            margin: '0 auto',
-            textAlign: 'center'
-          }
-        }}
-        tokens={stackHorizontalGap}
-      >
-        <CreateShortUrlComponent />
-      </Stack>
+      <h1>Hello World!!</h1>
     </ApolloProvider>
   );
 };
