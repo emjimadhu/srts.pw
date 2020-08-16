4b03005f5d1fcbb27382596061f2c64db7062c8b
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-17 00:18:58 +0530

    feat(client): :sparkles: add react router to home page

--- apps/client/src/app/app.component.tsx
+++ apps/client/src/app/app.component.tsx
@@ -1,67 +1,28 @@
 import React, { useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { Grid } from '@material-ui/core';
 import {
-  makeStyles, Theme, createStyles
-} from '@material-ui/core/styles';
+  Router, Switch, Route
+} from 'react-router-dom';
 
 import {
   apolloClient, getUser, setUser
 } from '@srts.pw/client/services/core';
 import { ClientComponentsHeader } from '@srts.pw/client/components/header';
-import {
-  ClientComponentsCreateUrl, IUrlDocument
-} from '@srts.pw/client/components/create-url';
-import { ClientComponentsUrlPreview } from '@srts.pw/client/components/url-preview';
-
-
-const useStyles = makeStyles((theme: Theme) => {
-  const height = `calc(98vh - ${theme.breakpoints.up('xs') ? '64px' : '54px'})`;
-  const space = (value: number) => {
-    return theme.spacing(value);
-  };
-
-  return createStyles({
-    root: {
-      marginTop: space(1),
-      height
-    },
-    urlInputContainer: {
-      height,
-      padding: space(2)
-    }
-  });
-});
+import { ClientPagesHome } from '@srts.pw/client/pages/home';
 
 const AppComponent: React.FC = () => {
-  const classes = useStyles();
-  const [
-    createdUrl,
-    setCreatedUrl
-  ] = useState<IUrlDocument | undefined>();
-
   if (!getUser()) {
     setUser();
   }
 
-  console.log('Created Url');
-  console.log(createdUrl);
-
   return (
     <ApolloProvider client={apolloClient}>
       <ClientComponentsHeader />
-      <Grid container className={classes.root}>
-        <Grid item xs={12}>
-          <Grid container alignItems="center" justify="center" alignContent="center" className={classes.urlInputContainer}>
-            <ClientComponentsCreateUrl
-              setCreatedUrl={setCreatedUrl}
-            />
-            <ClientComponentsUrlPreview
-              createdUrl={createdUrl}
-            />
-          </Grid>
-        </Grid>
-      </Grid>
+      <Switch>
+        <Route exact path="/">
+          <ClientPagesHome />
+        </Route>
+      </Switch>
     </ApolloProvider>
   );
 };
