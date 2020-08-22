7eda95e93f91e6b60e5b72429e980edf03d9bc0a
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-22 21:51:20 +0530

    feat(server-libs): :sparkles: send verify email on register

--- libs/server/user/src/lib/services/user-register/user-register.service.ts
+++ libs/server/user/src/lib/services/user-register/user-register.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { ServerCoreMailerService } from '@srts.pw/server/core/mailer';
 
 import { UserRepository } from '../../user.repository';
 import { UserRegisterInput } from './user-register.input';
@@ -9,12 +12,24 @@ import { UserType } from '../../user.type';
 export class UserRegisterService {
   constructor(
     @InjectRepository(UserRepository)
-    private readonly userRepository: UserRepository
+    private readonly userRepository: UserRepository,
+    private mailerService: ServerCoreMailerService
   ) {}
 
   public async register(
     requestVariables: UserRegisterInput
   ): Promise<UserType> {
-    return new UserType(await this.userRepository.register(requestVariables));
+    const verificationToken = uuid();
+
+    const userDocument = await this.userRepository.register(requestVariables, verificationToken);
+
+    await this.mailerService.sendVerificationTokenEmail({
+      email: userDocument.email,
+      firstName: userDocument.firstName,
+      lastName: userDocument.lastName,
+      token: `https://srts.pw/verify-email/${verificationToken}`
+    });
+
+    return new UserType(userDocument);
   }
 }
