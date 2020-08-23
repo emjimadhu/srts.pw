81a4012bafdecaf0b6f93b7e46e53f93d0319c25
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-23 23:42:34 +0530

    refactor(server-libs): :recycle: move `register` logic from UserRepository to UserRegisterService

--- libs/server/user/src/lib/services/user-register/user-register.service.ts
+++ libs/server/user/src/lib/services/user-register/user-register.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { v4 as uuid } from 'uuid';
 
+import {
+  UserRoles, generateSalt, hashPassword
+} from '@srts.pw/server/common/types';
 import { ServerCoreMailerService } from '@srts.pw/server/core/mailer';
 
 import { UserRepository } from '../../user.repository';
@@ -19,9 +22,30 @@ export class UserRegisterService {
   public async register(
     requestVariables: UserRegisterInput
   ): Promise<UserType> {
+    const {
+      firstName, lastName, email, password
+    } = requestVariables;
+    const salt = await generateSalt();
     const verificationToken = uuid();
 
-    const userDocument = await this.userRepository.register(requestVariables, verificationToken);
+    const userDocument = await this.userRepository.userRegister({
+      profile: {
+        firstName,
+        lastName
+      },
+      userName: email,
+      email,
+      password: await hashPassword(password, salt),
+      isVerified: false,
+      services: {
+        verificationToken: {
+          token: verificationToken,
+          generatedAt: new Date()
+        }
+      },
+      id: uuid(),
+      role: UserRoles.USER
+    });
 
     await this.mailerService.sendVerificationTokenEmail({
       email: userDocument.email,
--- libs/server/user/src/lib/user.repository.ts
+++ libs/server/user/src/lib/user.repository.ts
@@ -1,44 +1,16 @@
-import { EntityRepository } from 'typeorm';
-import { v4 as uuid } from 'uuid';
+import {
+  DeepPartial, EntityRepository
+} from 'typeorm';
 
 import { BaseRepository } from '@srts.pw/server/common/typeorm';
-import {
-  UserRoles, generateSalt, hashPassword
-} from '@srts.pw/server/common/types';
 
 import { User } from './user.entity';
-import { UserRegisterInput } from './services/user-register';
 
 @EntityRepository(User)
 export class UserRepository extends BaseRepository<User> {
-  public async register(requestVariables: UserRegisterInput, verificationToken?: string): Promise<User> {
-    const {
-      firstName, lastName, email, password
-    } = requestVariables;
-
-    const salt = await generateSalt();
-
-    const user = this.create({
-      profile: {
-        firstName,
-        lastName
-      },
-      userName: email,
-      email,
-      password: await hashPassword(password, salt),
-      isVerified: false,
-      services: {
-        verificationToken: {
-          token: verificationToken,
-          generatedAt: new Date()
-        }
-      },
-      id: uuid(),
-      role: UserRoles.USER
-    });
-
-
-    return this.save(user);
+  public async userRegister(user: DeepPartial<User>): Promise<User> {
+    const userDocument = this.create(user);
+    return this.save(userDocument);
   }
 
   public userReadByVerificationToken(token: string): Promise<User> {
