2d2d86d3bc49359ac1e866d62d49f93222fa2aba
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-23 10:07:17 +0530

    feat(client-libs): :sparkles: add `verify-email` logic

--- libs/client/pages/verify-email/src/lib/client-pages-verify-email.component.tsx
+++ libs/client/pages/verify-email/src/lib/client-pages-verify-email.component.tsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, {
+  useState, useEffect
+} from 'react';
 import {
-  Grid, CircularProgress, Typography
+  Grid, CircularProgress, Typography, Collapse
 } from '@material-ui/core';
 import {
   makeStyles, Theme, createStyles
 } from '@material-ui/core/styles';
-import { useParams } from 'react-router-dom';
+import {
+  Alert, AlertTitle
+} from '@material-ui/lab';
+import {
+  useParams, useHistory
+} from 'react-router-dom';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import {
+  useMutation, FetchResult
+} from '@apollo/client';
 
+import { AppRoutes } from '@srts.pw/client/types';
 import './client-pages-verify-email.component.scss';
+
+import {
+  USER_VERIFY_EMAIL_MUTATION, IUserVerifyEmail_ResponseData
+} from './verify-email.mutation';
 
 const useStyles = makeStyles((theme: Theme) => {
   const height = `calc(98vh - ${theme.breakpoints.up('xs') ? '64px' : '54px'})`;
@@ -38,37 +53,101 @@ export const ClientPagesVerifyEmail: React.FC = (properties: IClientPagesVerifyE
   const [
     isVerified,
     setIsVerified
-  ] = useState(true);
+  ] = useState(false);
   const [
     displayMessage,
     setDisplayMessage
   ] = useState('Verifying your Email...');
-  const parameters = useParams();
+  const {
+    token
+  } = useParams<{token: string}>();
+  const {
+    push
+  } = useHistory();
+  const [
+    error,
+    setError
+  ] = useState(false);
+  const [
+    errorMessage,
+    setErrorMessage
+  ] = useState('');
+
+  const [userVerifyEmailMutation] = useMutation(USER_VERIFY_EMAIL_MUTATION);
+
+  console.log('Normal', token);
+
+  useEffect(() => {
+    const sendRequest = async() => {
+      try {
+        const fetchResult: FetchResult<IUserVerifyEmail_ResponseData> = await userVerifyEmailMutation({
+          variables: {
+            token
+          }
+        });
+
+        if (!fetchResult.errors) {
+          setIsVerified(true);
+          setDisplayMessage('Email successfully Verified. Redirecting to Login');
+          setError(false);
+          setTimeout(() => {
+            push(AppRoutes.LOGIN);
+          }, 500);
+        } else {
+          setError(true);
+          const fetchResultErrorMessage = fetchResult.errors[0].message;
+          setErrorMessage(fetchResultErrorMessage.includes('duplicate key') ? 'Email already exsists' : fetchResultErrorMessage);
+        }
+      } catch(fetchError) {
+        setError(true);
+        (fetchError.message === 'Failed to fetch') ? setErrorMessage('Can\'t fetch from Server. Please try again later') : setErrorMessage(fetchError.message);
+      }
+    };
+
+    console.log(token);
+
+    sendRequest();
+  }, [token]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Grid container alignItems="center" justify="center" alignContent="center" direction="column" className={classes.container}>
-      <Grid item className={classes.progress}>
-        {
-          !isVerified ? (
-            <CircularProgress size="74px" />
-          ) : (
-            <CheckCircleIcon color="primary" className={classes.icon} />
-          )
-        }
-      </Grid>
-      <Grid item>
-        {
-          !isVerified ? (
-            <Typography
-              variant="h4"
-            >
-              {displayMessage}
-            </Typography>
-          ) : (
-            <div></div>
-          )
-        }
-      </Grid>
+      {
+        !error ? (
+          <>
+            <Grid item className={classes.progress}>
+              {
+                !isVerified ? (
+                  <CircularProgress size="74px" />
+                ) : (
+                  <CheckCircleIcon color="primary" className={classes.icon} />
+                )
+              }
+            </Grid>
+            <Grid item>
+              {
+                !isVerified ? (
+                  <Typography
+                    variant="h4"
+                  >
+                    {displayMessage}
+                  </Typography>
+                ) : (
+                  <div></div>
+                )
+              }
+            </Grid>
+          </>
+        ) : (
+          <Grid item>
+            <Collapse in={error}>
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                <strong>{errorMessage}</strong>
+              </Alert>
+            </Collapse>
+          </Grid>
+        )
+      }
     </Grid>
   );
 };
